203c7598d6fc3393194e35b363a241936dcbb9aa
Hazel_K <59563487+Hazel-K@users.noreply.github.com>
2020-06-13 22:34:41 +0900

    Update main.py

--- code11/main.py
+++ code11/main.py
@@ -5,3 +5,8 @@ URL = "https://www.fastcampus.co.kr/dev_online_introdev/#row_course"
 
 rq = rquests.get(URL)
 soup = bs(rq.content, 'html.parser')
+# 개발자 도구를 통한 웹사이트 분석
+
+# 크롤링 코드 작성
+
+# 파일 저장 및 데이터 분석
